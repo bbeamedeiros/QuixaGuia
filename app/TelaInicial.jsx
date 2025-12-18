7088b9c34e0c24e441eabcc910dfb4bc5771b73e
Natan Henrique Oliveira Façanha <natanh2016@gmail.com>
2025-12-18 14:47:03 +0000

    ajeitando a nav bar

--- app/TelaInicial.jsx
+++ app/TelaInicial.jsx
@@ -1,14 +1,13 @@
 import { router } from 'expo-router';
 import React from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
-import { Text, TextInput, Button, useTheme, Appbar, Searchbar } from 'react-native-paper';
+import { Text, TextInput, Button, useTheme, Appbar, Searchbar, BottomNavigation, Drawer } from 'react-native-paper';
 import { useFonts, TiltWarp_400Regular } from '@expo-google-fonts/tilt-warp';
 import {
     Urbanist_400Regular,
     Urbanist_500Medium,
     Urbanist_700Bold
 } from '@expo-google-fonts/urbanist';
-
 
 const { width } = Dimensions.get('window');
 
@@ -20,9 +19,10 @@ export default function Home() {
         Urbanist_700Bold,
     });
 
+//botando a appBar com a Search bar dentro
     const AppBar = () => (
   <Appbar.Header style={styles.appbar}>
-    <Appbar.BackAction onPress={() => {}} />
+    <Appbar.Action icon="menu" color="#F4FFE1" onPress={() => {}} />
 <SearchBar/>
 
   </Appbar.Header>
@@ -41,32 +41,84 @@ const SearchBar= () => {
   );
 };
 
+
+//aqui já começa a navbar
+const TelaInicial = () => (
+    <View style={styles.container}>
+         <AppBar/>
+        <View style={styles.infos}>
+        <Text style={styles.textoPequeno}>R. Basílio Emiliano Pinto, 243</Text>  
+            <Text style={styles.titulo}>Descubra</Text>
+            <Text style={styles.textoPequeno}>Fique por dentro de eventos futuros</Text>
+            </View>
+     </View>
+)
+const ImoveisRoute = () => (
+    <View style={styles.container}>
+         <AppBar/>
+        <View style={styles.infos}>
+            <Text style={styles.titulo}>Imóveis</Text>
+            </View>
+     </View>
+)
+
+const TransporteRoute = () => <Text>Recents</Text>;
+
+const LugaresRoute = () => <Text>Notifications</Text>;
+
+const BottomNav= () => {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'home', title: 'Home', focusedIcon: 'heart', unfocusedIcon: 'heart-outline'},
+    { key: 'imoveis', title: 'Imóveis', focusedIcon: 'album' },
+    { key: 'transporte', title: 'Transporte', focusedIcon: 'history' },
+    { key: 'lugares', title: 'Lugares', focusedIcon: 'bell', unfocusedIcon: 'bell-outline' },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    home: TelaInicial,
+    imoveis: ImoveisRoute,
+    transporte: TransporteRoute,
+    lugares: LugaresRoute,
+  });
+
+  return (
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+      barStyle={styles.navBar}  
+      activeColor="#F4FFE1"    // cor quando selecionado
+      inactiveColor= "#D4EFB1"   // cor quando NÃO selecionado
+    />
+  );
+};
+
+//aqui vem a renderização na tela
  return (
     
     <View style={styles.container}>
-        <AppBar/>
-        <View style={styles.infos}>
-        <Text style={styles.textoPequeno}>R. Basílio Pinto nº 243</Text>
-        </View>
-        <View style={styles.infos}>
-            <Text style={styles.titulo}>Descubra</Text>
-            </View>
+       
+            <BottomNav/>
      </View>
+
+
  )
+ 
 }
 
+
+//styles
 const styles = StyleSheet.create({
     container: {
         width: width, // Garante que ocupa a largura total da página no ScrollView
         flex: 1,
         alignItems: 'left',
-        
         backgroundColor: '#F4FFE1',
     },
     appbar:{
         backgroundColor: '#21582B',
         width: width,
-        
     },
     barraPesquisa:{
         backgroundColor: '#D4Efb1',
@@ -75,15 +127,21 @@ const styles = StyleSheet.create({
     },
     textoPequeno: {
         fontFamily: 'Urbanist_400Regular',
-        fontSize: 16
+        fontSize: 16,
 
     },
     titulo: {
         fontFamily: 'TiltWarp_400Regular',
         color: '#9D1B1B',
-        fontSize: 36
+        fontSize: 36,
+  
     },
     infos: {
-    
+        paddingLeft: 28,
+        paddingTop: 28
+    },
+    navBar: {
+         backgroundColor: '#21582B',
+ 
     }
 })
